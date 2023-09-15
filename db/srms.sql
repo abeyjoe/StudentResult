596f4018a0044ecee6d50ca8b01726c0293d247c
Abiodun Adedapo <abeyjoe18@gmail.com>
2023-09-15 16:34:12 +0100

    StudentResult management with PHP

---
+++ db/srms.sql
@@ -0,0 +1,255 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 18, 2022 at 07:28 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `srms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2020-06-11 12:26:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclasses`
+--
+
+CREATE TABLE `tblclasses` (
+  `id` int(11) NOT NULL,
+  `ClassName` varchar(80) DEFAULT NULL,
+  `ClassNameNumeric` int(4) NOT NULL,
+  `Section` varchar(5) NOT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblclasses`
+--
+
+INSERT INTO `tblclasses` (`id`, `ClassName`, `ClassNameNumeric`, `Section`, `CreationDate`, `UpdationDate`) VALUES
+(1, 'First', 1, 'FSC', '2022-07-04 09:10:32', '0000-00-00 00:00:00'),
+(2, 'Second', 2, 'FFMS', '2022-07-04 09:11:04', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblresult`
+--
+
+CREATE TABLE `tblresult` (
+  `id` int(11) NOT NULL,
+  `StudentId` int(11) DEFAULT NULL,
+  `ClassId` int(11) DEFAULT NULL,
+  `SubjectId` int(11) DEFAULT NULL,
+  `marks` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblresult`
+--
+
+INSERT INTO `tblresult` (`id`, `StudentId`, `ClassId`, `SubjectId`, `marks`, `PostingDate`, `UpdationDate`) VALUES
+(1, 1, 1, 2, 100, '2022-07-04 09:18:48', NULL),
+(2, 1, 1, 1, 100, '2022-07-04 09:18:48', NULL),
+(3, 1, 1, 3, 99, '2022-07-04 09:18:48', NULL),
+(4, 3, 1, 2, 85, '2022-07-04 09:19:07', NULL),
+(5, 3, 1, 1, 95, '2022-07-04 09:19:07', NULL),
+(6, 3, 1, 3, 96, '2022-07-04 09:19:07', NULL),
+(7, 2, 2, 2, 84, '2022-07-04 09:19:20', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `StudentId` int(11) NOT NULL,
+  `StudentName` varchar(100) NOT NULL,
+  `RollId` varchar(100) NOT NULL,
+  `StudentEmail` varchar(100) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `DOB` varchar(100) NOT NULL,
+  `ClassId` int(11) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `Status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblstudents`
+--
+
+INSERT INTO `tblstudents` (`StudentId`, `StudentName`, `RollId`, `StudentEmail`, `Gender`, `DOB`, `ClassId`, `RegDate`, `UpdationDate`, `Status`) VALUES
+(1, 'Adedapo Abiodun', '123', 'abeyjoe18@gmail.com', 'Male', '2022-07-05', 1, '2022-07-04 09:14:00', NULL, 1),
+(2, 'John Doe', '122', 'johndoe@gmail.com', 'Male', '2022-07-05', 2, '2022-07-04 09:14:49', NULL, 1),
+(3, 'Olu Daniel', '133', 'oludaniel@gmail.com', 'Male', '2022-07-05', 1, '2022-07-04 09:17:06', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubjectcombination`
+--
+
+CREATE TABLE `tblsubjectcombination` (
+  `id` int(11) NOT NULL,
+  `ClassId` int(11) NOT NULL,
+  `SubjectId` int(11) NOT NULL,
+  `status` int(1) DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updationdate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblsubjectcombination`
+--
+
+INSERT INTO `tblsubjectcombination` (`id`, `ClassId`, `SubjectId`, `status`, `CreationDate`, `Updationdate`) VALUES
+(1, 1, 1, 1, '2022-07-04 09:17:42', '2022-07-04 09:17:42'),
+(2, 1, 2, 1, '2022-07-04 09:17:49', '2022-07-04 09:17:49'),
+(3, 1, 3, 1, '2022-07-04 09:17:57', '2022-07-04 09:17:57'),
+(4, 2, 2, 1, '2022-07-04 09:18:04', '2022-07-04 09:18:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubjects`
+--
+
+CREATE TABLE `tblsubjects` (
+  `id` int(11) NOT NULL,
+  `SubjectName` varchar(100) NOT NULL,
+  `SubjectCode` varchar(100) NOT NULL,
+  `Creationdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblsubjects`
+--
+
+INSERT INTO `tblsubjects` (`id`, `SubjectName`, `SubjectCode`, `Creationdate`, `UpdationDate`) VALUES
+(1, 'Introduction To Computer', 'COM 111', '2022-07-04 09:11:59', '0000-00-00 00:00:00'),
+(2, 'Digital Electronics', 'COM 112', '2022-07-04 09:12:15', '0000-00-00 00:00:00'),
+(3, 'O O BASIC', 'COM113', '2022-07-04 09:12:31', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblclasses`
+--
+ALTER TABLE `tblclasses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblresult`
+--
+ALTER TABLE `tblresult`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  ADD PRIMARY KEY (`StudentId`);
+
+--
+-- Indexes for table `tblsubjectcombination`
+--
+ALTER TABLE `tblsubjectcombination`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblsubjects`
+--
+ALTER TABLE `tblsubjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblclasses`
+--
+ALTER TABLE `tblclasses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblresult`
+--
+ALTER TABLE `tblresult`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  MODIFY `StudentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblsubjectcombination`
+--
+ALTER TABLE `tblsubjectcombination`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblsubjects`
+--
+ALTER TABLE `tblsubjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Student Result Management System
+Database Name: srms
+
+Admin Page: http://localhost/StudentResult/
+Username: admin
+Password: admin
+
